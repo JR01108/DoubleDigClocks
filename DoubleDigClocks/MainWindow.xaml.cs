d5d67a1cb17576e84e417328314b770f57d9eb08
Даниил <@DESKTOP-NH3ATFH>
2022-10-10 19:45:02 +0300

    Добавьте файлы проекта.

---
+++ DoubleDigClocks/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoubleDigClocks
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        Time time;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            time = new Time();
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowTime();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ShowTime();
+        }
+        private void ShowTime()
+        {
+            TimeText.Text = time.getBinTime();            
+            DecimalTime.Text = time.getDecTime();
+        }
+
+        private void DecimalTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ChangeFormatButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ChangeFormatButton.Content as string == "AM/PM")
+            {
+                ChangeFormatButton.Content = "24H";
+            }
+            else
+            {
+                ChangeFormatButton.Content = "AM/PM";
+            }
+            time.ChangeFormat();
+        }
+    }
+}
---
+++ DoubleDigClocks/Time.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleDigClocks
+{
+    internal class Time
+    {
+        public int h;
+        public int m;
+        public int s;
+        public int format;
+
+        public Time()
+        {
+            format = 24;
+        }
+
+        public void ChangeFormat()
+        {
+            if (format == 24)
+                format = 12;
+            else
+                format = 24;
+        }
+
+        public string getDecTime()
+        {
+            string time = "";
+            if (format == 24)
+            {
+                h = DateTime.Now.Hour;
+                m = DateTime.Now.Minute;
+                s = DateTime.Now.Second;
+
+                if (h < 10)
+                {
+                    time += "0" + h;
+                }
+                else
+                {
+                    time += h;
+                }
+
+                time += ":";
+
+                if (m < 10)
+                {
+                    time += "0" + m;
+                }
+                else
+                {
+                    time += m;
+                }
+
+                time += ":";
+
+                if (s < 10)
+                {
+                    time += "0" + s;
+                }
+                else
+                {
+                    time += s;
+                }
+            }
+            else
+            {
+                h = DateTime.Now.Hour;
+                m = DateTime.Now.Minute;
+                s = DateTime.Now.Second;
+                if (h < 13)
+                {
+                    time += "AM ";
+                }
+                else
+                {
+                    time += "PM ";
+                }
+
+                if ((h == 0)||(h == 12))
+                {
+                    h = 12;
+                }
+                else
+                {
+                    h = (h + 1) % 13;
+                }
+                if (h < 10)
+                {
+                    time += "0" + h;
+                }
+                else
+                {
+                    time += h;
+                }
+
+                time += ":";
+
+                if (m < 10)
+                {
+                    time += "0" + m;
+                }
+                else
+                {
+                    time += m;
+                }
+
+                time += ":";
+
+                if (s < 10)
+                {
+                    time += "0" + s;
+                }
+                else
+                {
+                    time += s;
+                }
+            }
+
+            return time;
+        }
+
+        private string getBinaryNumber(int length, int number)
+        {
+            return Convert.ToString(number, 2).PadLeft(length, '0');
+        }
+
+        public string getBinTime()
+        {
+            string time = "";
+            time = getBinaryNumber(5, DateTime.Now.Hour) + ':'
+                + getBinaryNumber(6, DateTime.Now.Minute) + ':'
+                + getBinaryNumber(6, DateTime.Now.Second);
+            return time;
+        }
+    }
+}
